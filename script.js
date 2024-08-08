677d558566e65e6f0eac747be1066c9ed1fa0943
Gulnaz Abdiashim <159135738+abdiashim06@users.noreply.github.com>
2024-08-08 18:21:16 +0600

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ character = "World";
 let secondCharacter;
 secondCharacter = character;
 console.log(secondCharacter);
+
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
