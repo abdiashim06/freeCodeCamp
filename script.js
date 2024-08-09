a7aa3157a68f050942e9714754c1bacd9a4a98df
Gulnaz Abdiashim <159135738+abdiashim06@users.noreply.github.com>
2024-08-09 20:29:17 +0600

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,74 @@
-let character = 'Hello';
+t character = 'Hello';
 console.log(character);
 character = "World";
+console.log(character);
 let secondCharacter;
-secondCharacter = character;
+secondCharacter = "Test"
 console.log(secondCharacter);
+secondCharacter= character;
+
+
+let profession;
+let age;
+profession = "teacher";
+
+console.log(profession);
+console.log(age);
+
 
 
 let character = 'Hello';
 let count = 8;
+// console.log(count + 1);
+// console.log(count - 1);
+// console.log(count * 1);
+// console.log(count / 1);
+// let rows = [];
 let rows = ["Naomi", "Quincy", "CamperChan"];
 console.log(rows[0]);
+rows[2] = 10;
+console.log(rows);
+rows[rows.length - 1];
+rows = 10;
+
+let cities = ["London", "New York", "Mumbai"];
+console.log(cities);
+cities[cities.length - 1] = "Mexico City";
+console.log(cities);
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp")
+console.log(rows);
+let popped = rows.pop();
+console.log(popped);
+
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed)
+
+let character = "Hello";
+let count = 8;
+let rows = [];
+
+const character = "Hello";
+const count = 8;
+const rows = [];
+
+let character = "Hello";
+character = "#"
+const count = 8;
+const rows = [];
+
+for("iterator"; "condition"; "iteration") {
+}
+
+for (let i = 0; "condition"; "iteration") {
+
+}
+
+for (let i = 0; i < count; "iteration") {
+
+}
+
+for (let i = 0; i < count; i = i + 1) {
+
+}
